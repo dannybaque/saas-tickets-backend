7de3bcd22ea8882a8a121dccec5f4368b0563e0e
dannybaque <baquedanny9@gmail.com>
2026-03-09 22:25:43 -0500

    feat: login y lista de tickets funcionando

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 const express = require('express')
-const app = express()
+const cors = require('cors')
 require('dotenv').config()
 
+const app = express()
+
+app.use(cors())
 app.use(express.json())
 
 const authRoutes = require('./routes/auth')
-app.use('/api/auth', authRoutes)
-
 const ticketRoutes = require('./routes/tickets')
+
+app.use('/api/auth', authRoutes)
 app.use('/api/tickets', ticketRoutes)
 
 app.get('/', (req, res) => {
